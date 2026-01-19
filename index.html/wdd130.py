05971fb1fd80a7602334a41a80869f1846206728
kateelijah <ubikateelijah@gmail.com>
2026-01-19 10:33:16 -0800

    Add WDD130 homepage and profile image

---
+++ index.html/wdd130.py
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Kate Elijah Ubi | WDD 130</title>
+</head>
+
+<body>
+<header>
+<nav>
+<a href="#">Home</a>
+<a href="wwr/">Rafting Website</a>
+</nav>
+</header>
+
+<main>
+<h1>Kate Elijah Ubi | WDD 130</h1>
+<img src="images/profile.jpg" alt="Hello, my name is Kate Elijah Ubi" width="200">
+<p>
+Hello! My name is Kate Elijah Ubi. I am learning how to build websites in WDD 130 and I enjoy learning new technologies and web design.
+</p>
+</main>
+
+<footer>
+<p>©2026 🌴 Kate Elijah Ubi 🌴 Nigeria</p>
+</footer>
+</body>
+</html>
